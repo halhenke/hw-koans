300c66991cc387822c131c59d4dc370c944533a8
Hal Henke <halhenke@gmail.com>
2017-07-13 13:34:51 +1000

    The Parser stuff so far

--- koan/Koan/Parser.hs
+++ koan/Koan/Parser.hs
@@ -8,7 +8,7 @@ import Data.Semigroup
 import Prelude hiding (fail)
 
 enrolled :: Bool
-enrolled = False
+enrolled = True
 
 data ParseResult a
   = ParseSuccess String a
@@ -16,37 +16,56 @@ data ParseResult a
   deriving (Eq, Show)
 
 instance Functor ParseResult where
-  fmap = error "TODO: Implement fmap for ParseResult"
+  fmap f (ParseSuccess s m) = ParseSuccess s (f m)
+  fmap f (ParseFailure s) = ParseFailure s
 
 newtype Parser a = Parser
   { runParser :: String -> ParseResult a
   }
 
 fail :: String -> Parser a
-fail = error "TODO: Implement fail"
+fail message = Parser $ const (ParseFailure message)
+-- fail message = Parser $ const $ ParseFailure message
 
 instance Functor Parser where
-  fmap = error "TODO: Implement fmap for Parser"
+  -- (String -> Int) -> Parser String -> Parser Int
+  -- fmap f (Parser rp) = Parser (f . rp)
+  -- fmap f (Parser rp) = Parser (fmap f rp)
+  -- fmap f (Parser rp) = Parser $ (fmap <$> fmap) f rp
+  fmap f (Parser rp) = Parser (\s -> f <$> rp s)
 
 instance Applicative Parser where
-  pure a  = error "TODO: Implement pure for Parser"
-  (<*>)   = error "TODO: Implement (<*>) for Parser"
+  pure a  = Parser (\s -> ParseSuccess s a)
+  -- pure a  = Parser (\s -> ParseSuccess s a)
+  (<*>) (Parser pf) (Parser pa) = Parser $ \s -> case (pf s) of
+    (ParseSuccess str a_to_b) -> case (pa str) of
+      (ParseSuccess str an_a) -> ParseSuccess str (a_to_b an_a)
+      (ParseFailure str) -> ParseFailure str
+    (ParseFailure str) -> ParseFailure str
+    -- otherwise -> expression
 
 instance Alternative Parser where
-  empty = error "TODO: Implement empty for Parser"
-  (<|>) = error "TODO: Implement (<|>) for Parser"
+  empty = fail "empty"
+  Parser pa <|> Parser pb = Parser $ \s -> case pa s of
+    (ParseSuccess str a) -> (ParseSuccess str a)
+    (ParseFailure str) -> pb str
 
 satisfy :: (Char -> Bool) -> Parser Char
-satisfy = error "TODO: Implement satisfy"
+satisfy run = let
+  go [] = ParseFailure "Incomplete"
+  go (c:cs) = if (run c)
+    then ParseSuccess cs c
+    else ParseFailure "satisfyWith"
+  in Parser go
 
 char :: Char -> Parser Char
-char c = error "TODO: Implement char"
+char c = satisfy (== c)
 
 notChar :: Char -> Parser Char
-notChar c = error "TODO: Implement notChar"
+notChar c = satisfy (/= c)
 
 anyChar :: Parser Char
-anyChar = error "TODO: Implement anyChar"
+anyChar = satisfy (const True)
 
 skip :: (Char -> Bool) -> Parser ()
 skip p = error "TODO: Implement skip"
